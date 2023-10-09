75669559dff01e818858fc1c01cc092e3eabf4b0
Tejas Sawant <115860482+Tejaso7@users.noreply.github.com>
2023-10-09 18:38:35 +0530

    new files added by ts

--- scripts/ani_demo.js
+++ scripts/ani_demo.js
@@ -10,19 +10,35 @@ document.addEventListener('DOMContentLoaded', function () {
     lineColor: '#5cbdaa'
   });
   var intro = document.getElementById('intro');
-  intro.style.MarginTop = - intro.offsetHeight / 2 + 'px';
+  intro.style.MarginTop = - intro.offsetHeight / 1 + 'px';
 }, false);
 
+ /**
+//    * Hero type effect
+//    */
+//  const typed = select('.typed')
+//  if (typed) {
+//    let typed_strings = typed.getAttribute('data-typed-items')
+//    typed_strings = typed_strings.split(',')
+//    new Typed('.typed', {
+//      strings: typed_strings,
+//      loop: true,
+//      typeSpeed: 100,
+//      backSpeed: 50,
+//      backDelay: 2000
+//    });
+//  }
 
-/*
+
+// /*
 // jQuery plugin example:
-$(document).ready(function() {
-  $('#particles').particleground({
-    dotColor: '#5cbdaa',
-    lineColor: '#5cbdaa'
-  });
-  $('.intro').css({
-    'margin-top': -($('.intro').height() / 2)
-  });
-});
-*/
+// $(document).ready(function() {
+//   $('#particles').particleground({
+//     dotColor: '#5cbdaa',
+//     lineColor: '#5cbdaa'
+//   });
+//   $('.intro').css({
+//     'margin-top': -($('.intro').height() / 2)
+//   });
+// });
+// */
